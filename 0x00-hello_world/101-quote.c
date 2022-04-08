7fa1210615c9ebf684c6231f215c9c59fdb1d4a0
wangui-ann <wangui.ann8498@gmail.com>
2022-04-07 18:57:22 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - prints exactly "and that piece of art is useful"
  *  - Dora Korpar, 2015-10-19,
  * followed by a new line to the standard error
- * Return: always 0 (success)
+ * Return: always 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
