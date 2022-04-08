f4b044ee0ae6e135c5796329ad69d0d221d4deba
wangui-ann <wangui.ann8498@gmail.com>
2022-04-07 17:58:32 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art, \n");
+	printf("With proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
