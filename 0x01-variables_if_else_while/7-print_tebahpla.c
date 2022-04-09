427e19d2e144c45a264c07617a32af2dfae1e6e0
wangui-ann <wangui.ann8498@gmail.com>
2022-04-08 18:49:02 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in reverse,
+ * followed by a new line, using putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char(ch)
+	{
+		for (ch = 'a'; ch <= 'z'; ch--)
+		{
+			putchar(ch)
+		}
+		putchar('\n');
+		return (0);
+	}
+}
