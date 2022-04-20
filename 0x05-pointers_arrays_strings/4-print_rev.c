dd570084e4e69959c71273e9b6968d332f9db914
wangui-ann <wangui.ann8498@gmail.com>
2022-04-20 12:36:51 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * followed by a new line
+ * @s: this is the pointer of the string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char s;
+
+	while (s > 0)
+	{
+		_putchar(s);
+		s--;
+	}
+	_putchar('\n');
+}
