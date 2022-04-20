bbe91a9e26bc39dc9e2f41876e241d7a1e292c2b
wangui-ann <wangui.ann848@gmail.com>
2022-04-20 09:31:42 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int: swaps the values of two integers
+ * @a: this is pointer 1
+ * @b: this is pointer 2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c;
+
+	c = *a;
+	*b = *a;
+	*b = c;
+}
