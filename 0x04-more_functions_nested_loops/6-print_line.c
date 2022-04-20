d49973347f836070435ba9bddf15cd56d9d26a39
wangui-ann <wangui.ann848@gmail.com>
2022-04-19 19:24:48 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * followed by a new line
+ * @n: an input integer
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
