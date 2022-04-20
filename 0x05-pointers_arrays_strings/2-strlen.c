44c618ce4d589c8f9b71b52805acef95c068eb66
wangui-ann <wangui.ann848@gmail.com>
2022-04-20 09:46:53 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: This is the pointer of the string to be checked
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
