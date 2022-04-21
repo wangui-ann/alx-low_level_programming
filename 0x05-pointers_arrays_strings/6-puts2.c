2f3399f1af235829aad3f1dd64c35deceaef1cf8
wangui-ann <wangui.ann8498@gmail.com>
2022-04-20 18:06:55 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
