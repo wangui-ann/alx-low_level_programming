bfb82d2abe011d127e531f0986d9c29b29c3c712
wangui-ann <wangui.ann8498@gmail.com>
2022-04-26 14:34:04 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - main function for setting the memory
+ * @s: this is the pointer
+ * @b: this is the character
+ * @n: this is the unsigned character
+ * Return: return the memory of s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset n, s, b;
+
+	return (s);
+}
