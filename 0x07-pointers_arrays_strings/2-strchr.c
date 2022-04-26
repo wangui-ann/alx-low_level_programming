83606f4211e011f56909102a56afb57ff487b612
wangui-ann <wangui.ann8498@gmail.com>
2022-04-26 15:03:53 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - returns a pointer to the first occurence of a character
+ * @c: this is the character to be searched
+ * @s: this is the string
+ * Return: c, or NULL if character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *ret = strchr(s, c);
+	
+	return (ret);
+}
