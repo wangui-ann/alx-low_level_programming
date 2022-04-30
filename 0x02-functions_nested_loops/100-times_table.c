b997af24882c8d9ffccbf7ea97b6c8f7a6c534eb
wangui-ann <wangui.ann8498@gmail.com>
2022-04-29 21:20:54 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,6 @@ void print_times_table(int n)
 
 				if (column == 0)
 					_putchar('0');
-
 				else if (product < 10)
 				{
 					_putchar(',');
@@ -29,8 +28,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product % 10 + '0');
 				}
-
-				else if (product >=10 && product < 100)
+				else if (product >= 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -38,7 +36,6 @@ void print_times_table(int n)
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
 				}
-
 				else if (product > 99 && product < 1000)
 				{
 					_putchar(',');
