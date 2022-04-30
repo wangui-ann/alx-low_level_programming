763a1f1f34eaed5590bdf2d51a1b527c00f40866
wangui-ann <wangui.ann8498@gmail.com>
2022-04-29 19:55:32 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This is the main function
+ * @s: This is the string that has been entered
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
