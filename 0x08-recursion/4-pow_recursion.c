b30bbf4570b1b160873cb8464e838d144f91dba4
wangui-ann <wangui.ann8498@gmail.com>
2022-04-29 20:07:38 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This is the main gives the length of string
+ * @x: This is the interger that has been entered
+ * @y: This is the interger that has been entered
+ * Return: always success
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
+}
