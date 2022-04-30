d184b889cf69d9d48ad88906ad31037d6643f2b2
wangui-ann <wangui.ann8498@gmail.com>
2022-04-29 20:00:59 -0700

    Task 2

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/***factorial - This is the main gives the length of string
+ * @n: This is the interger that has been entered
+ * Return: always success
+ */
+
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	result *= factorial(n - 1);
+
+	return (result);
+}
