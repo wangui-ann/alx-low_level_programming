5a855fd7f5317e01562a56a0cb446e63ef737dec
wangui-ann <wangui.ann8498@gmail.com>
2022-05-06 18:37:29 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	gridout = malloc(height * sizeof(int *));
 	if (gridout == NULL)
 	{
-		gree(gridout);
+		free(gridout);
 		return (NULL);
 	}
 
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 			for (i--; i >= 0; i--)
 				free(gridout[i]);
 			free(gridout);
-			return (NLL);
+			return (NULL);
 		}
 	}
 
