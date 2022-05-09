41f57ebf978a6f4556124ad46f4cd80a574e6c5d
wangui-ann <wangui.ann8498@gmail.com>
2022-05-08 19:09:53 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,7 @@
  * main - prints all arguments it receives.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return 0: success
+ * Return: 0 success
  */
 
 int main(int argc, char *argv[])
