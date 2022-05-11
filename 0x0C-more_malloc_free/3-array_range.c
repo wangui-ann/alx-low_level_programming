710501bcb39af04fb017c1b5d2810621b710e7af
wangui-ann <wangui.ann8498@gmail.com>
2022-05-10 17:39:58 -0700

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,8 +15,8 @@ int *array_range(int min, int max)
 	arr = malloc((max - min + 1) * sizeof(int));
 
 	if (!arr)
-		return(0);
-	while (i <=max - min)
-		ar[i++] +t++;
+		return (0);
+	while (i <= max - min)
+		arr[i++] = t++;
 	return (arr);
 }
