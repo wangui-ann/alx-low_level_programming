6e9740bbd750c7c33453ac04111dce028f77fe83
wangui-ann <wangui.ann8498@gmail.com>
2022-05-10 17:32:25 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -32,7 +32,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-		p = malloc(nmemb * size);
+	p = malloc(nmemb * size);
+
 	if (p == NULL)
 		return (NULL);
 	_memset(p, 0, (nmemb * size));
