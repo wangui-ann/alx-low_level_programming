a383956dda5227fc4169bbc0da6e5b60e045beeb
wangui-ann <wangui.ann8498@gmail.com>
2022-05-16 20:12:17 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - function that prints a name
  * @name: name to be printed
- * @f: pointer to thee function
+ * @f: pointer to the function
  */
 
 void print_name(char *name, void (*f)(char *))
