985972e5cf73437ab2bb812236ed4e30c9a458ef
wangui-ann <wangui.ann8498@gmail.com>
2022-05-16 18:46:01 -0700

    Task 0: function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @p: pointer to thee function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name)
+}
