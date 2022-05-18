574bfb379f518bbd6a752e12bde939fb4d1a3afd
wangui-ann <wangui.ann8498@gmail.com>
2022-05-17 17:35:43 -0700

    Variadic functions header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+/**
+ * File: VARIADIC_FUNCTIONS
+ * Author: wangui-ann
+ * Description: header files that contains all functions of the program
+ */
+
+int _putchar(void);
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
