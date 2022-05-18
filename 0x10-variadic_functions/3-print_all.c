721dd3264dd32b4b9569d112ddf4f3328dbdc31c
wangui-ann <wangui.ann8498@gmail.com>
2022-05-17 19:54:39 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,15 +2,71 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * print_all - function that prints everything
- * @format: a list of types of arguments
- */
-
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - prints a character
+ * @arg: list of arguments pointing to char
+ */
+void print_char(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, int);
+	printf("%c", c);
+}
+
+/**
+ * print_int - prints an integer
+ * @arg: list of arguments pointing to int
+ */
+void print_int(va_list arg)
+{
+	int i;
+
+	i = va_arg(arg, int);
+	printf("%d", i);
+}
+
+/**
+ * print_float - prints a float
+ * @arg: list of arguments pointing to float
+ */
+void print_float(va_list arg)
+{
+	float f;
+
+	f = va_arg(arg, double);
+	printf("%f", double);
+}
+
+/**
+ * print_string - prints a string
+ * @arg: a lit of arguments pointing to string
+ */
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)")
+			return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - prints anything, followed by a new line
+ * @format: alist of types of arguments
+ */
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
