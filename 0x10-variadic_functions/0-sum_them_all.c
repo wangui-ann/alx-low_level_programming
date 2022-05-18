0f1cf2a7de5f74bbb4e0bf2eb712b5dd60d0ffa3
wangui-ann <wangui.ann8498@gmail.com>
2022-05-17 17:58:32 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list nums;
 	unsigned int i, sum = 0;
 
-	va_start(nums, i);
+	va_start(nums, n);
 
-	for (i = 0; i < sum; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(nums, int);
-	va_end(args);
+	va_end(nums);
 	return (sum);
 	if (n == 0)
 		return (0);
