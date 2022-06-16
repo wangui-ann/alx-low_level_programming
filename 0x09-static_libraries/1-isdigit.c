0d738550951f682415ee9aec87743f5c0d2a67ec
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 01:23:52 +0300

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,7 +1,14 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
 #include "main.h"
-int main(void)
+/**
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isdigit(int c)
 {
-	int _isdigit(int c);
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
 }
