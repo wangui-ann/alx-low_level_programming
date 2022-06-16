7cae11b83816ee3bd3ca2cfdba2670487f6cb08a
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:46:28 +0300

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,7 +1,20 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
 #include "main.h"
-int main(void)
+/**
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *_memset(char *s, char b, unsigned int n);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
