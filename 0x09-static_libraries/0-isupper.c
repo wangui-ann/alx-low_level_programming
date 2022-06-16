356802940bf397b8d8ce7ae9a1cc258cc2b95d41
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:45:48 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,7 +1,14 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
 #include "main.h"
-int main(void)
+/**
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
 {
-	int _isupper(int c);
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+
+	return (0);
 }
