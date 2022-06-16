cdef5650875c14a31055f38fd15517beb0e4c77b
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:49:57 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,7 +1,20 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
-#include "main.h"
-int main(void)
+/**
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
+int _atoi(char *s)
 {
-	int _atoi(char *s);
+int i = 1;
+unsigned int num = 0;
+do {
+if (*s == '-')
+i *= -1;
+else if (*s >= '0' && *s <= '9')
+num = num * 10 + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *i);
 }
