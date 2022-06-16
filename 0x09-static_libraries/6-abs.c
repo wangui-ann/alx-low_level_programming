270da7e52bcb8fcc5b93ec9833340d786b1e84f4
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:59:33 +0300

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,6 +1,15 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
-void fun(void)
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@i: number targeted
+*Return: returns 0
+*/
+int _abs(int i)
 {
-	int _abs(int n);
+if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
+else
+return (0);
 }
