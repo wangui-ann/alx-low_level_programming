7bc980be10208535dcbd0a8c9d69cb4862e2e83e
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 01:00:15 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,6 +1,18 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
-void fun(void)
+#include "main.h"
+/**
+* _strcpy - Copy paste string
+*@dest: destination
+*@src: source
+*Return: dest
+*/
+char *_strcpy(char *dest, char *src)
 {
-	char *_strcpy(char *dest, char *src);
+int inc = 0;
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\0';
+return (dest);
 }
