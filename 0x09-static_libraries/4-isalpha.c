6e2dc739d361fc8ffff864bc4e007f119ae6642f
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:57:13 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,6 +1,13 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
-void fun(void)
+#include "main.h"
+/**
+*_isalpha - controls if a character is alphabetical
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int _isalpha(int c)
 {
-	int _isalpha(int c);
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+
+return (0);
 }
