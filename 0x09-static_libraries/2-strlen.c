155e8e366d019c68b35ec15d2bdc81edf299966f
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:51:26 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,6 +1,15 @@
-/* Filename: lib_mylib.c */
-#include <stdio.h>
-void fun(void)
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
 {
-	int _strlen(char *s);
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
 }
