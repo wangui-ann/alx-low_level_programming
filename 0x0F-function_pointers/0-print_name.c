b71ccfdcf0f20337198f24ee1315c05abc55890c
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-18 08:01:23 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name
- * @name: name to be printed
- * @f: pointer to the function
+ * print_name - a function that prints a name.
+ * @name: pointer to name to print
+ * @f: function to print name
+ * Return: Nothing
  */
-
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *name))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
