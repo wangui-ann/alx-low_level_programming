8bed076998c30074ec7cb0fd94310d63b1d79562
Ann <93719602+wangui-ann@users.noreply.github.com>
2023-01-18 12:43:45 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,11 @@
 #include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function generates a djb2
+ * @str: pointer to chars a hash
+ * Return: hash key
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
