f0db312106c9e86159f67f7e5f4fefcd60b3b0ff
Ann <93719602+wangui-ann@users.noreply.github.com>
2023-01-18 12:46:39 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * keyindex - hash function generates a djb2
+ * keyindex - hash function
  * @str: pointer to chars a hash
- * Return: hash key
+ * Return: index
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
