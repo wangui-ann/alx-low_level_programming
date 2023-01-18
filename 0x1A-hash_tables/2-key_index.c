d16523a9c3a0980e8ee575f205410729af8cf84f
wangui-ann <wangui.ann8498@gmail.com>
2023-01-18 01:25:12 -0800

    remove files

--- 0x1A-hash_tables/2-key_index.c
+++
@@ -1,5 +0,0 @@
-#include "hash_tables.h"
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	return (hash_djb2(key) % size);
-}
--- 0x1A-hash_tables/README.md
+++
@@ -1,2 +0,0 @@
-# 0x1A-hash_tables
-### Max Stuart 2019 October 10
